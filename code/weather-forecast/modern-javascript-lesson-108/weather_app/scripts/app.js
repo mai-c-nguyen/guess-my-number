27b41b02fba22e6c45102e19de25cdcfa98e4f27
Mai Nguyen <mai.nguyen.gl@gmail.com>
2021-05-12 23:11:13 +0200

    HTML JS CSS

--- code/weather-forecast/modern-javascript-lesson-108/weather_app/scripts/app.js
+++ code/weather-forecast/modern-javascript-lesson-108/weather_app/scripts/app.js
@@ -1,4 +1,3 @@
-
 const cityForm = document.querySelector('form');
 const card = document.querySelector('.card');
 const details = document.querySelector('.details');
@@ -6,13 +5,9 @@ const time = document.querySelector('img.time');
 const icon = document.querySelector('.icon img');
 
 const updateUI = (data) => {
+  // destructure properties
+  const { cityDets, weather } = data;
 
-  // const cityDets = data.cityDets;
-  // const weather = data.weather;
-
-// destructure properties
-
-const { cityDets, weather } = data;
   // update details template
   details.innerHTML = `
     <h5 class="my-3">${cityDets.EnglishName}</h5>
@@ -20,43 +15,45 @@ const { cityDets, weather } = data;
     <div class="display-4 my-4">
       <span>${weather.Temperature.Metric.Value}</span>
       <span>&deg;C</span>
-  `
+    </div>
+  `;
+
   // update the night/day & icon images
-
-const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
-icon.setAttribute('src', iconSrc);
-
+  const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
+  icon.setAttribute('src', iconSrc);
+  
   let timeSrc = null;
   if(weather.IsDayTime){
     timeSrc = 'img/day.svg';
   } else {
     timeSrc = 'img/night.svg';
   }
-
   time.setAttribute('src', timeSrc);
 
-  // remove the d-none
+  // remove the d-none class if present
   if(card.classList.contains('d-none')){
     card.classList.remove('d-none');
-  };
+  }
 };
 
-
 const updateCity = async (city) => {
+
   const cityDets = await getCity(city);
   const weather = await getWeather(cityDets.Key);
-  return { cityDets, weather: weather };
+  return { cityDets, weather };
+
 };
 
 cityForm.addEventListener('submit', e => {
   // prevent default action
   e.preventDefault();
+  
   // get city value
   const city = cityForm.city.value.trim();
   cityForm.reset();
 
-  // update the ul with new city
+  // update the ui with new city
   updateCity(city)
-  .then(data => updateUI(data))
-  .catch(err => console.log(err));
+    .then(data => updateUI(data))
+    .catch(err => console.log(err));
 });
