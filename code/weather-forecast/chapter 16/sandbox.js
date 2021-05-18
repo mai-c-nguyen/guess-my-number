e82b21ed665ea3f5dfb96ee7ed406a96dc4e1881
MaiAn <usuario@MacBook-Air-de-Usuario.local>
2021-05-18 11:07:18 +0200

    JS HTML CSS

---
+++ code/weather-forecast/chapter 16/sandbox.js
@@ -0,0 +1,18 @@
+<script>
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyDu_g-Alj4Oz-6f5e8G7K7sOYqEBf3tui8",
+    authDomain: "mai-c-nguyen-javascript.firebaseapp.com",
+    databaseURL: "https://mai-c-nguyen-javascript-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "mai-c-nguyen-javascript",
+    storageBucket: "mai-c-nguyen-javascript.appspot.com",
+    messagingSenderId: "646936334874",
+    appId: "1:646936334874:web:3162006008e0c0f7df37a3",
+    measurementId: "G-XGEJT5FK7E"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+  const db = firebase.firestore();
+</script>
